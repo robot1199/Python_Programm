224cc5cb21cd3b8d4ba817935c1462aa11c177df
robot1199 <kolechkoalexander@gmail.com>
2022-10-01 16:56:13 +0300

    4_1_6

---
+++ code/lesson_4_1_6.py
@@ -0,0 +1,23 @@
+class VideoRating:
+    def __init__(self):
+        self.__rating = 0
+
+    @property
+    def rating(self):
+        return self.__rating
+
+    @rating.setter
+    def rating(self, value):
+        if type(value) != int or not (0 <= value <= 5):
+            raise ValueError('неверное присваиваемое значение')
+        self.__rating = value
+
+class VideoItem():
+    def __init__(self, title, descr, path):
+        self.title = title
+        self.descr = descr
+        self.path = path
+
+
+v = VideoItem('Курс по Python ООП', 'Подробный курс по Python ООР', 'D:/videos/python_oop.mp4')
+print(v.rating.rating) # 0
