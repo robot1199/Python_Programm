904f9df688e99dc8c73bdfb7aa82da1356222bcc
robot1199 <kolechkoalexander@gmail.com>
2022-10-19 20:22:59 +0300

    4_5_6

---
+++ code/lesson_4_4_11.py
@@ -0,0 +1,35 @@
+CURRENT_OS = 'windows'  # 'windows', 'linux'
+
+
+class WindowsFileDialog:
+    def __init__(self, title, path, exts):
+        self.__title = title  # заголовок диалогового окна
+        self.__path = path  # начальный каталог с файлами
+        self.__exts = exts  # кортеж из отображаемых расширений файлов
+
+
+class LinuxFileDialog:
+    def __init__(self, title, path, exts):
+        self.__title = title  # заголовок диалогового окна
+        self.__path = path  # начальный каталог с файлами
+        self.__exts = exts  # кортеж из отображаемых расширений файлов
+
+
+class FileDialogFactory:
+    def __new__(cls, *args, **kwargs):
+        if CURRENT_OS == 'windows':
+            return cls.create_windows_filedialog(args[0], args[1], args[2])
+        else:
+            return cls.create_linux_filedialog(args[0], args[1], args[2])
+
+
+    @staticmethod
+    def create_windows_filedialog(title, path, exts):
+        return WindowsFileDialog(title, path, exts)
+
+    @staticmethod
+    def create_linux_filedialog(title, path, exts):
+        return LinuxFileDialog(title, path, exts)
+
+
+dlg = FileDialogFactory('Изображения', 'd:/images/', ('jpg', 'gif', 'bmp', 'png'))
